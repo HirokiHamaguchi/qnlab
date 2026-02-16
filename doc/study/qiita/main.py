ddbae1fab5e2d78a69e1af26203e933236b60e89
Hiroki Hamaguchi <hari64boli64@gmail.com>
2026-02-16 22:54:14 +0900

    add index

--- doc/study/qiita/main.py
+++ doc/study/qiita/main.py
@@ -25,21 +25,7 @@ def main() -> None:
     convert_pdf_to_png(current_dir.parent / "imgs" / "quasi_newton")
     convert_pdf_to_png(current_dir.parent / "imgs" / "modified_secant")
 
-    all_markdown_lines = ["<!-- markdownlint-disable MD041 -->"]
-
-    main_file = "0_main.tex"
-    main_content = ""
-    with open(current_dir / main_file, "r", encoding="utf-8") as f:
-        main_content = f.read()
-        main_content = main_content[
-            main_content.rfind("\\else") + 6 : main_content.rfind("\\fi")
-        ]
-        main_content = "\n".join(line.strip() for line in main_content.splitlines())
-        all_markdown_lines.extend([f"\n<!-- From {main_file} -->\n", main_content])
-
-    all_markdown_lines.append(
-        '\n<img width="100%" src="https://raw.githubusercontent.com/HirokiHamaguchi/qnlab/master/doc/imgs/quasi_newton/sixhump.png?v=1" />\n'
-    )
+    all_markdown_lines = []
 
     tex_files = [f for f in sorted(current_dir.glob("[1-4]*.tex"))]
     assert tex_files, "No .tex files found in current directory."
@@ -60,6 +46,49 @@ def main() -> None:
             "以上です。お読みいただきありがとうございました。",
         ]
     )
+
+    top_sentences = ["<!-- markdownlint-disable MD041 -->"]
+
+    main_file = "0_main.tex"
+    main_content = ""
+    with open(current_dir / main_file, "r", encoding="utf-8") as f:
+        main_content = f.read()
+        main_content = main_content[
+            main_content.rfind("\\else") + 6 : main_content.rfind("\\fi")
+        ]
+        main_content = "\n".join(line.strip() for line in main_content.splitlines())
+        top_sentences.extend([f"\n<!-- From {main_file} -->\n", main_content])
+
+    top_sentences.append(
+        '\n<img width="100%" src="https://raw.githubusercontent.com/HirokiHamaguchi/qnlab/master/doc/imgs/quasi_newton/sixhump.png?v=1" />\n'
+    )
+
+    top_sentences.extend(
+        [
+            "## 目次",
+            "",
+            "本記事はかなり網羅的かと思われます。興味のある箇所からお読みください。",
+            "",
+        ]
+    )
+
+    for line in all_markdown_lines:
+        if line.startswith("#"):
+            heading_level = line.count("#")
+            heading_text = line[heading_level:].strip()
+            escaped_heading_text = (
+                heading_text.replace("(", "")
+                .replace(")", "")
+                .replace(" ", "-")
+                .replace("–", "")
+            )
+            top_sentences.append(
+                f"{'  ' * (heading_level - 2)}- [{heading_text}]({'#' + escaped_heading_text})"
+            )
+
+    top_sentences.append("\n---\n")
+
+    all_markdown_lines = top_sentences + all_markdown_lines
 
     output_file = current_dir / "qiita" / "combined_output.md"
     output_content = "\n".join(all_markdown_lines)
